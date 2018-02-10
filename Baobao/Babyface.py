8b41e2f8d41cf77de514eca01f5da3f8e36189a3
En Yi <randomsity1@gmail.com>
2018-02-10 15:50:36 +0000

    Animate face

--- Baobao/Babyface.py
+++ Baobao/Babyface.py
@@ -7,20 +7,24 @@ Created on Sat Feb 03 14:00:22 2018
 import os, sys
 import pygame
 from pygame.locals import *
-from win32api import GetSystemMetrics
+#from win32api import GetSystemMetrics
 #from Babyclass import *
-
+from screeninfo import get_monitors
+import myRecorder as mR
 #Get screensize
 #x= GetSystemMetrics(0)*0.8
-y= int(GetSystemMetrics(1)*0.8)
+#y= int(GetSystemMetrics(1)*0.8)
+mon = get_monitors()
+y = mon[0].height;
 SCREENRECT     = Rect(0, 0, y, y)
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 def load_image(file):
+    #global y
     "loads an image, prepares it for play"
     file = os.path.join(main_dir, file)
-    y= int(GetSystemMetrics(1)*0.8)
+    #y= int(GetSystemMetrics(1)*0.8)
     try:
         surface = pygame.image.load(file)
         surface=pygame.transform.scale(surface, (y,y))
@@ -28,10 +32,11 @@ def load_image(file):
         raise SystemExit('Could not load image "%s" %s'%(file, pygame.get_error()))
     return surface.convert()
 
-def load_images(*files):
+def load_images(path):
     imgs = []
-    for file in files:
-        imgs.append(load_image(file))
+    for file in os.listdir(path):
+        if '.png' in file:
+            imgs.append(load_image(path+'/'+file))
     return imgs
 
 class Baby(pygame.sprite.Sprite):
@@ -45,18 +50,48 @@ class Baby(pygame.sprite.Sprite):
     speak = []
     wakeup = []
 
-    def __init__(self):
+    def __init__(self, fps = 2):
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.image=self.smile
+        self.frames=self.smile
+        self.current = 0
+        self.image = self.frames[0]
+        print(self.frames)
+        print(self.image)
         self.rect = self.image.get_rect(midbottom=SCREENRECT.midbottom)
+        self._next_update = 0    # next time it has to be updated in ms
+        self._period = 1000./fps # period of the animation in ms
+        self._inv_period = 1./self._period
+        self._paused_time = 0
+        self._pause_start = 0
+        self._frames_len = len(self.frames)
+
     def changeFace(self):
-        if self.image==self.smile:
-            self.image=self.sad
+        if self.frames==self.smile:
+            self.frames=self.sad
         else:
-            self.image=self.smile
-        self.rect = self.image.get_rect(midbottom=SCREENRECT.midbottom)
-    #def update(self):
-        
+            self.frames=self.smile
+
+        self.current = 0
+        self._frames_len = len(self.frames)
+
+    def update(self, dt,t):
+        if self._next_update <= t:
+            # time past since it should have updated
+            delta = t - self._paused_time - self._next_update
+            # calculate if there are any skipped frames
+            skipped_frames = int(delta*self._inv_period)
+            # next time to update
+            self._next_update = self._next_update + self._period + skipped_frames * self._period
+            # update to next image
+            self.current += (1+skipped_frames)
+            # bind it to the length of the animation
+            self.current %= self._frames_len
+
+            self.image = self.frames[self.current]
+            self.rect = self.image.get_rect(midbottom=SCREENRECT.midbottom)
+        #self.current += 1
+        #if self.current == len(self.frames):
+        #    self.current = 0
 
 
 # main window function
@@ -71,26 +106,26 @@ def main(winstyle = 0):
     winstyle = 0  # |FULLSCREEN
     bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
     screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
-    
+
     #Load images, assign to sprite classes
     #(do this before the classes are used, after screen setup)
-    path='Expression/'
-    Baby.smile = load_image(path+'smile.gif')
-    Baby.sad = load_image(path+'sad.gif')
-    Baby.bored = load_image(path+'bored.gif')
-    Baby.sleep = load_image(path+'sleep.gif')
-    Baby.sleepy = load_image(path+'sleepy.gif')
-    Baby.cry = load_image(path+'cry.gif')
-    Baby.laugh = load_image(path+'laugh.gif')
-    Baby.speak = load_image(path+'speak.gif')
-    Baby.wakeup = load_image(path+'wakeup.gif')
+    path='./Faces/'
+    Baby.smile = load_images(path+'smile')
+    Baby.sad = load_images(path+'sad')
+    Baby.bored = load_images(path+'bored')
+    Baby.sleep = load_images(path+'sleep')
+    Baby.sleepy = load_images(path+'sleepy')
+    Baby.cry = load_images(path+'cry')
+    Baby.laugh = load_images(path+'laugh')
+    Baby.speak = load_images(path+'speak')
+    Baby.wakeup = load_images(path+'wakeup')
 
     #decorate the game window
     icon = pygame.transform.scale(load_image('icon.png'), (32, 32))
     pygame.display.set_icon(icon)
     pygame.display.set_caption('BaoBao')
     #pygame.mouse.set_visible(0)
-    
+
     #create the background, tile the bgd image
     bgdtile = load_image('background.jpg')
     background = pygame.Surface(SCREENRECT.size)
@@ -98,28 +133,42 @@ def main(winstyle = 0):
         background.blit(bgdtile, (x, 0))
     screen.blit(background, (0,0))
     pygame.display.flip()
-    
+
     #Initialise baby
     all = pygame.sprite.RenderUpdates()
     Baby.containers=all
     Baobao=Baby()
     clock = pygame.time.Clock()
-    
+
+    rec = mR.Recorder(num_chunk = 1,
+                      device_name = 'HDA Intel PCH: ALC3236 Analog (hw:1,0)')
+    rec.stream_init()
+    getTicksLastFrame = pygame.time.get_ticks()
     #Game loop
     while 1:
         # get input
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                del rec
                 pygame.quit()
                 return
         # clear/erase the last drawn sprites
         all.clear(screen, background)
+
+        t = pygame.time.get_ticks()
+        dt = (t - getTicksLastFrame) / 1000.0
+        getTicksLastFrame = t
+
         #update all the sprites
-        all.update()
-        
+        all.update(dt,t)
+
         keystate = pygame.key.get_pressed()
         #handle player input
-        firing = keystate[K_SPACE]
+        #firing = keystate[K_SPACE]
+        audio_data = rec.get_buffer()
+        print(max(abs(audio_data))[0])
+        firing = max(abs(audio_data))> 0.1
+
         if firing:
             Baobao.changeFace()
 
--- Baobao/RecorderParent.py
+++ Baobao/RecorderParent.py
@@ -2,17 +2,17 @@
 #Created on Sat Jul 15 20:01:58 2017
 #@author: En Yi
 """
-This module contains the abstract class to implement a proper    
+This module contains the abstract class to implement a proper
 Recorder Class. To do so, subclass RecorderParent when creating
 a new Recorder class.
 
 Example:
     from RecorderParent import RecorderParent
-    
+
     class newRecorder(RecorderParent):
         ...
         ...
-        
+
 If you have PyQt, it will import RecEmitter for emitting Signals.
 
 Attributes
@@ -30,13 +30,13 @@ try:
 except Exception as e:
     print(e)
     QT_EMITTER = False
-    
+
 
 
 class RecorderParent(object):
     """
     Recorder abstract class. Sets up the buffer and skeleton for audio streaming
-    
+
     Attributes
     ----------
     channels: int
@@ -51,13 +51,13 @@ class RecorderParent(object):
         Indicate whether to record
     """
     __metaclass__ = ABCMeta
-    
-#---------------- INITIALISATION METHODS -----------------------------------    
+
+#---------------- INITIALISATION METHODS -----------------------------------
     def __init__(self,channels = 1,rate = 44100, chunk_size = 1024,
                  num_chunk = 4):
         """
         Initialise a ciruclar buffer, array and trigger for recording
-        
+
         Parameters
         ----------
         channels: int
@@ -74,17 +74,17 @@ class RecorderParent(object):
         self.chunk_size = chunk_size
         self.num_chunk = num_chunk;
         self.audio_stream = None #: The audio object
-        
+
         self.allocate_buffer()
         self.show_stream_settings()
-        
+
         self.trigger_init()
-        
+
         if QT_EMITTER:
             self.rEmitter = RecEmitter()
         else:
             self.rEmitter = None
-                 
+
     def allocate_buffer(self):
         """
         Set up the circular buffer
@@ -94,17 +94,17 @@ class RecorderParent(object):
                                         self.channels))
         self.next_chunk = 0;
 
-#---------------- DESTRUCTOR METHODS -----------------------------------     
+#---------------- DESTRUCTOR METHODS -----------------------------------
     def __del__(self):
         self.close()
-           
+
     def close(self):
         """
-        Close the audio object, to be called if streaming is no longer needed 
+        Close the audio object, to be called if streaming is no longer needed
         """
         self.stream_close()
- 
-#---------------- DEVICE SETTINGS METHODS -----------------------------------     
+
+#---------------- DEVICE SETTINGS METHODS -----------------------------------
     def show_stream_settings(self):
         """
         Show the settings of the recorder
@@ -113,70 +113,70 @@ class RecorderParent(object):
         print('Rate: %i' % self.rate)
         print('Chunk size: %i' % self.chunk_size)
         print('Number of chunks: %i' % self.num_chunk)
-        
+
     def set_filename(self,filename):
         self.filename = filename
-        
+
     @abstractmethod
     def set_device_by_name(self, name):
         """
         Set the device to be used for audio streaming
         """
         pass
-    
-    @abstractmethod    
+
+    @abstractmethod
     def available_devices(self):
         """
         Displays all available device for streaming
         """
         pass
-    
+
     @abstractmethod
     def current_device_info(self):
         """
         Displays information about available the current device set
         """
         pass
-    
+
 #---------------- DATA METHODS -----------------------------------
     def audiodata_to_array(self,data):
         """
         Convert audio data obtained into a proper array
-        
+
         Parameters
         -----------
         data: Numpy Array
-            Audio data 
+            Audio data
         """
         return data.reshape((-1,self.channels))/ 2**15
-    
+
 #---------------- BUFFER METHODS -----------------------------------
     def write_buffer(self,data):
         """
         Write the data obtained into buffer and move to the next chunk
-        
+
         Parameters
         -----------
         data: Numpy Array
-            Audio data 
+            Audio data
         """
         self.buffer[self.next_chunk,:,:] = data
         self.next_chunk = (self.next_chunk + 1) % self.num_chunk
-     
+
     def get_buffer(self):
         """
         Convert the buffer data as a 2D array by stitching the chunks together
-        
+
         Returns
         ----------
         Buffer data: Numpy Array
             with dimension of(chunk_size * num_chunk) x channels
-            The newest data on the most right 
+            The newest data on the most right
         """
         return np.concatenate((self.buffer[self.next_chunk:],self.buffer[:self.next_chunk]),axis = 0) \
                  .reshape((self.buffer.shape[0] * self.buffer.shape[1],
                            self.buffer.shape[2]))
-        
+
 #---------------- RECORDING METHODS -----------------------------------
     def open_recorder(self):
         """
@@ -187,14 +187,14 @@ class RecorderParent(object):
         self.next_rec_chunk = 0
         self.total_rec_chunk = 0
         self.recorded_data = []
-        
+
     def record_init(self,samples = None,duration = 3):
         """
         Remove any pretrigger and postrigger data
         Calculate the number of chunk to record
         It will record more samples than necessary, then slice down to the
         amount of samples required + putting in pretrigger data
-        
+
         Parameters
         ----------
         samples: int
@@ -204,30 +204,30 @@ class RecorderParent(object):
         """
         if not self._record_check():
             return False
-        
+
         self.pretrig_data = np.array([],dtype = np.int16)
         self.part_posttrig_data = np.array([],dtype = np.int16)
         if samples:
             self.actual_rec_samples = samples
             self.total_rec_chunk = (samples // self.chunk_size)+1
-            self.rec_samples = self.total_rec_chunk * self.chunk_size 
+            self.rec_samples = self.total_rec_chunk * self.chunk_size
         else:
             self.total_rec_chunk = int(duration * self.rate // self.chunk_size)
-            self.rec_samples = self.total_rec_chunk * self.chunk_size 
+            self.rec_samples = self.total_rec_chunk * self.chunk_size
             self.actual_rec_samples = self.rec_samples
-            
+
         self.next_rec_chunk = 0
-        
+
         self.initialised_record = True
-        
+
         print('Recording function is ready! Use record_start() to start')
-        
+
         return True
-    
+
     def _record_check(self):
         """
         Check if it is possible to start a recording
-        
+
         Returns
         ----------
         bool
@@ -236,19 +236,19 @@ class RecorderParent(object):
         if not self.audio_stream:
             print('No recording stream initiated!')
             return False
-        
+
         # Check if the previous recorded data is flushed
         if self.recorded_data:
             print('Please flush your recorded data')
             return False
-        
+
         return True
 
     # Function to initiate a normal recording
     def record_start(self):
         """
         Start recording if it is possible
-        
+
         Returns
         ----------
         bool
@@ -256,9 +256,9 @@ class RecorderParent(object):
         """
         if not self._record_check():
             return False
-                       
+
         # Start the recording if recording is initialised
-        if self.initialised_record: 
+        if self.initialised_record:
             self.stream_start()
             self.recording = True
             print('Recording Start!')
@@ -266,7 +266,7 @@ class RecorderParent(object):
         else:
             print('Record not initialised! Use record_init(duration) first!')
             return False
-        
+
     def _record_stop(self):
         """
         Stop a successful recording and emit a signal if possible
@@ -277,7 +277,7 @@ class RecorderParent(object):
         print('Recording Done! Please flush the data with flush_record_data().')
         if self.rEmitter:
             self.rEmitter.recorddone.emit()
-            
+
     def record_cancel(self):
         """
         Cancel a recording and clear any recorder data
@@ -286,7 +286,7 @@ class RecorderParent(object):
         self.trigger = False
         self.recording = False
         self.recorded_data = []
-           
+
     def record_data(self,data):
         """
         Append recorded chunk to recorder_data
@@ -298,66 +298,66 @@ class RecorderParent(object):
         self.next_rec_chunk += 1
         if self.next_rec_chunk == self.total_rec_chunk:
             self._record_stop()
-          
+
     def flush_record_data(self):
         """
         Add in any partial posttrigger data
         Slice the recorded data into the requested amount of samples
         Add in any pretrigger data
-        
+
         Returns
         ----------
         flushed_data: numpy array
-            2D numpy array (similar to get_buffer) 
+            2D numpy array (similar to get_buffer)
         """
         if self.recorded_data:
             data =  np.array(self.recorded_data);
             flushed_data = data.reshape((self.rec_samples,self.channels))
             if self.part_posttrig_data.shape[0]:
                 flushed_data = np.vstack((self.part_posttrig_data,flushed_data))
-            
+
             flushed_data = flushed_data[:self.actual_rec_samples,:]
 
             self.recorded_data = []
             if self.pretrig_data.shape[0]:
                 flushed_data = np.vstack((self.pretrig_data,flushed_data))
-                
-            print('Data flushed')
-            return flushed_data               
-                            
 
-#---------------- STREAMING METHODS ----------------------------------- 
-    @abstractmethod                                    
+            print('Data flushed')
+            return flushed_data
+
+
+#---------------- STREAMING METHODS -----------------------------------
+    @abstractmethod
     def stream_init(self, playback = False):
         """
         Callback function for initialising audio streaming.
-                
+
         Parameters
         ----------
         playback: bool
             Whether to output the stream to a device
-            
+
         Returns
         ----------
         bool
             True if successful, False otherwise
         """
         pass
-            
-    @abstractmethod 
+
+    @abstractmethod
     def stream_start(self):
         """
         Callback function for starting the audio streaming.
         """
         pass
-    @abstractmethod 
+    @abstractmethod
     def stream_stop(self):
         """
         Callback function for stopping the audio streaming.
         """
         pass
-        
-    @abstractmethod 
+
+    @abstractmethod
     def stream_close(self):
         """
         Callback function for closing the audio streaming.
@@ -375,11 +375,11 @@ class RecorderParent(object):
         self.pretrig_samples = 200
         self.pretrig_data = np.array([])
         self.part_posttrig_data = np.array([])
-        
+
     def trigger_start(self,duration = 3, threshold = 0.09, channel = 0,pretrig = 200,posttrig = 5000):
         """
         Start the trigger if possible
-        
+
         Returns
         ----------
         bool
@@ -388,7 +388,7 @@ class RecorderParent(object):
         if self.recording:
             print('You are current recording. Please finish the recording before starting the trigger.')
             return False
-        
+
         if not self.trigger:
             if not self.record_init(samples = posttrig):
                 return False
@@ -408,7 +408,7 @@ class RecorderParent(object):
         """
         Check if the trigger is set off
         Start recording if so and emit a signal if possible
-        
+
         Parameters
         ----------
         data: Numpy Array
@@ -416,9 +416,9 @@ class RecorderParent(object):
         """
         trig_data = data[:,self.trigger_channel]
         norm_data = abs(trig_data - np.mean(trig_data))#- self.ref_level
-        
+
         maximum = np.amax(norm_data)
-        
+
         if maximum > self.trigger_threshold:
             print('Triggered!')
             pos = np.argmax(norm_data)
@@ -433,7 +433,7 @@ class RecorderParent(object):
                 print('Cannot get trigger data')
             if self.rEmitter:
                 self.rEmitter.triggered.emit()
-            
+
 #----------------- DECORATOR METHODS --------------------------------------
     @property
     def num_chunk(self):
@@ -457,12 +457,12 @@ class RecorderParent(object):
         except Exception as e:
             #print(e)
             self._num_chunks = n
-        
-        
+
+
     @property
     def chunk_size(self):
         """
-        int 
+        int
             Number of samples to get from each channel in one chunk
             The setter method will calculate the maximum possible size
             based on an arbitrary number of sample limit (2^25 in here)
